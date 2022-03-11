7c466ab6e4e9833a85b90144385a8679686f402a
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-03-10 22:20:38 -0500

    custom nested

--- questions/serializers.py
+++ questions/serializers.py
@@ -1,8 +1,11 @@
-from rest_framework import  serializers
+from rest_framework import serializers
 from .models import Question
+from core.serializers import UserSerializer
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
     class Meta:
         model = Question
         fields = '__all__'
